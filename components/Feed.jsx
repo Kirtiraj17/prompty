a5482ca09f7b60a3d9de6140877da048edea514d
Kirtiraj17 <kirtirajg1997@gmail.com>
2023-12-31 11:15:33 +0530

    search functionality add

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,20 +1,15 @@
 "use client";
-import { useState, useEffect } from "react";
-import PromptCard from "./PromptCard";
-
-const PromptCardList = ({ data, handleTagClick }) => (
-  <div className="mt-16 prompt_layout">
-    {data.map((post) => (
-      <PromptCard key={post._id} post={post} handleTagClick={handleTagClick} />
-    ))}
-  </div>
-);
+import { useState, useEffect, useCallback } from "react";
+import PromptCardList from "./PromptCardList";
 
 const Feed = () => {
   const [searchText, setSearchText] = useState("");
+  const [originalPosts, setOriginalPosts] = useState([]);
   const [posts, setPosts] = useState([]);
 
-  const handleSearchChange = (e) => {};
+  const handleSearchChange = (e) => {
+    setSearchText(e.target.value);
+  };
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -22,26 +17,52 @@ const Feed = () => {
       const response = await fetch(`/api/prompt?cacheBuster=${cacheBuster}`);
       const data = await response.json();
 
+      setOriginalPosts(data);
       setPosts(data);
     };
 
     fetchPosts();
   }, []);
 
+  const debouncedSearch = useCallback(() => {
+    let filteredPosts = originalPosts?.filter((p) => {
+      return (
+        p?.prompt?.toLowerCase().includes(searchText.toLowerCase()) ||
+        p?.tag?.toLowerCase().includes(searchText.toLowerCase()) ||
+        p?.creator?.username.toLowerCase().includes(searchText.toLowerCase())
+      );
+    });
+
+    setPosts(filteredPosts);
+  }, [searchText, originalPosts]);
+
+  useEffect(() => {
+    const timerID = setTimeout(() => debouncedSearch(), 500);
+    if (searchText === "") setPosts(originalPosts);
+
+    return () => clearTimeout(timerID);
+  }, [searchText, originalPosts, debouncedSearch]);
+
   return (
     <section className="feed">
       <form className="relative w-full flex-center">
         <input
           type="text"
-          placeholder="Search for a tag or username"
+          placeholder="Search for a prompt, tag or username"
           value={searchText}
-          onChange={handleSearchChange}
+          onChange={(e) => handleSearchChange(e)}
           required
           className="search_input peer"
         />
       </form>
 
-      <PromptCardList data={posts} handleTagClick={() => {}} />
+      {posts.length > 0 ? (
+        <PromptCardList data={posts} handleTagClick={() => {}} />
+      ) : (
+        <div className="my-8">
+          <p className="desc">No Posts Found!</p>
+        </div>
+      )}
     </section>
   );
 };
